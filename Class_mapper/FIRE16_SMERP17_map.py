565f928476320286c1e37f55fc0751c3c460a9fb
Samujjwal <cs16resch01001@iith.ac.in>
2020-06-09 18:58:06 +0530

    Added FIRE16 to SMERP17 class mapper

--- Class_mapper/FIRE16_SMERP17_map.py
+++ Class_mapper/FIRE16_SMERP17_map.py
@@ -26,20 +26,60 @@ __license__     : "This source code is licensed under the MIT-style license
                    source tree."
 """
 
+import numpy as np
+import pandas as pd
 
-def mapper(fire16_df, smerp17_df):
+
+def labels_mapper(df, class_maps: dict = None):
     """ Maps FIRE16 dataset labels to SMERP17 labels.
 
-    Class maps:
+    -1 denotes the column to be deleted.
+    Other columns are merged using logical or.
+
+    :param class_maps:
         FIRE16 -> SMERP17
             0       0
             1       1
             2       0
             3       1
-            4       -
+            4       -1
             5       3
             6       2
-
-    :param fire16_df:
-    :param smerp17_df:
+    :param df:
     """
+    if class_maps is None:
+        class_maps = {
+            2: 0,
+            3: 1,
+            # 4: -1,
+            5: 3,
+            6: 2
+        }
+    new_cols = sorted(list(class_maps.values()))
+    df2 = pd.DataFrame(columns=new_cols)
+    # df2 = df[df.columns.difference(new_cols)]
+    # df2['text'] = df['text']
+    df2.insert(loc=0, column='text', value=df['text'])
+    # df2 = df[not new_cols]
+    # for col in df.columns:
+    for cls, mapped_cls in class_maps.items():
+        df2[mapped_cls] = np.logical_or(df[cls], df[mapped_cls]) * 1
+        # if mapped_cls == -1:  ## Delete column
+        #     del df[cls]
+        # else:  ## Merge columns using OR:
+        #     df[mapped_cls] = np.logical_or(df[cls], df[mapped_cls]) * 1
+        #     del df[cls]
+
+    # df2.index = df.index
+    return df2
+
+
+def main():
+    a = [1, 0, 1]
+    b = [1, 0, 0]
+    out = np.logical_or(a, b) * 1
+    print(out)
+
+
+if __name__ == "__main__":
+    main()
