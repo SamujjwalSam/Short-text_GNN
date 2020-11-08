50fac84fb2589e86b0f52f814ccdca2bf7d53fe1
Samujjwal <cs16resch01001@iith.ac.in>
2020-11-08 18:56:45 +0530

    Corrected the direct S parameter update problem and copied GCN code

--- Layers/gcn_dropedgelearn.py
+++ Layers/gcn_dropedgelearn.py
@@ -37,6 +37,56 @@ def dot(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     return res
 
 
+class GraphConvolutionLayer(torch.nn.Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolutionLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' ('\
+               + str(self.in_features) + ' -> '\
+               + str(self.out_features) + ')'
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+        self.gc1 = GraphConvolutionLayer(nfeat, nhid)
+        self.gc2 = GraphConvolutionLayer(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
+
+
 class GCN_DropEdgeLearn_Layer(torch.nn.Module):
     """ GCN layer with DropEdge and learnable param matrix S. """
     def __init__(self, num_token, emb_dim, out_dim=2, adj_drop=0.0, bias=True):
@@ -70,7 +120,9 @@ class GCN_DropEdgeLearn_Layer(torch.nn.Module):
 
     def forward(self, A, X):
         torch.autograd.set_detect_anomaly(True)
-        A_prime = torch.mul(self.S, A)
+        D = self.get_adj_dropout(shape_A=A.shape)
+        D_prime = torch.mul(self.S, D)
+        A_prime = torch.mul(D_prime, A)
         support = torch.mm(X, self.weight)
         X = dot(A_prime, support)
 
@@ -79,15 +131,18 @@ class GCN_DropEdgeLearn_Layer(torch.nn.Module):
         else:
             return X
 
-    def apply_adj_dropout(self, adj_drop=0.2):
+    def get_adj_dropout(self, shape_A, adj_drop=0.2):
         """ Applies dropout to the whole adjacency matrix.
+
+        TODO: symmetric dropout (for undirected graph)
 
         :param adj_drop: dropout rate
         """
         ## Apply dropout to whole adjacency matrix:
-        # TODO: symmetric dropout (for undirected graph)
         adj_dropout = torch.nn.Dropout(p=adj_drop, inplace=False)
-        self.S = adj_dropout(self.S)
+        D = torch.ones(shape_A)
+        D_prime = adj_dropout(D)
+        return D_prime
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.num_token) + ')'\
